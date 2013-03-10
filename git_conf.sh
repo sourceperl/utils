f782049cb71c7fd97d156ee25c9436c3c654e17a
sourceperl <pi@raspberrypi.(none)>
2013-03-10 21:43:54 +0100

    remove install.sh, add git_conf.sh

---
+++ git_conf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# add color to git
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+
+# think to make...
+echo "don't forget to set :"
+echo "git config --global user.name \"your_pseudo\""
+echo "git config --global user.email moi@email.com"
