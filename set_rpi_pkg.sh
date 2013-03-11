a66ec12e3690729ffe2af0758a29c3350d79a681
sourceperl <pi@raspberrypi.(none)>
2013-03-11 21:43:48 +0100

    set_rpi_pkg.sh : add minicom to the list of install packet

--- set_rpi_pkg.sh
+++ set_rpi_pkg.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get update
 
-sudo apt-get install xscreensaver bc hexedit vim nmap gcc-avr avr-libc avrdude gnuplot-x11 proftpd lftp git
+sudo apt-get install xscreensaver bc hexedit vim nmap gcc-avr avr-libc avrdude gnuplot-x11 proftpd lftp git minicom
