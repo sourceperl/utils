d8165da060d2b834e765dc08d947dd4c1320cd1d
l.lefebvre <loic.celine@free.fr>
2014-03-01 14:11:09 +0100

    Create usefull_pkg.sh

---
+++ usefull_pkg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# Misc
+sudo apt-get install bc hexedit vim nmap gnuplot-x11 lftp git minicom
+
+# Arduino
+sudo apt-get install gcc-avr avr-libc avrdude
+
+# Node.js
+sudo apt-get install nodejs-legacy
+
+# FTP server
+sudo apt-get install proftpd
