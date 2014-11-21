626c2a131e20c3fb551b7e796c873d70b1591044
sourceperl <loic.celine@free.fr>
2014-11-21 13:08:14 +0100

    update usefull_pkg

--- usefull_pkg.sh
+++ usefull_pkg.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
+# update APT index
 sudo apt-get update
 
 # Misc
 sudo apt-get install bc hexedit vim nmap gnuplot-x11 lftp git minicom
 
 # Arduino
-sudo apt-get install gcc-avr avr-libc avrdude
+#sudo apt-get install gcc-avr avr-libc avrdude
 
 # Node.js
 sudo apt-get install nodejs-legacy
-
-# FTP server
-sudo apt-get install proftpd
 
 # Script "daemonize" like
 sudo apt-get install supervisor
 
 # Python modules
-sudo apt-get install python-pip python-setuptools python-serial python-scipy python-requests python-twisted ipython
+sudo apt-get install python-pip python-serial python-scipy \
+python-requests python-twisted ipython \
+python3-pip python3-serial python3-scipy python3-requests ipython3
+
+# Python lib
+sudo pip install pyModbusTCP --upgrade
+sudo pip-3.2 install pyModbusTCP --upgrade
+
+# FTP server
+sudo apt-get install proftpd
+# not start at system init
+sudo /etc/init.d/proftp stop
+sudo insserv -r proftpd
+
