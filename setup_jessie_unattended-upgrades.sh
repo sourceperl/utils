159afb7ae9ae1a09ae118f8ababf11149e8fdb4f
l.lefebvre <loic.celine@free.fr>
2016-02-14 19:19:27 +0100

    Create setup_jessie_unattended-upgrades.sh

---
+++ setup_jessie_unattended-upgrades.sh
@@ -0,0 +1,14 @@
+# add auto-update of debian jessie security packages
+
+# note: after do this /etc/cron.daily/apt will update package list daily, 
+# download all upgradeable packages and auto-upgrade security issues
+
+# add unattended-upgrades package
+sudo apt-get install unattended-upgrades
+
+# configure unattended-upgrades
+sudo touch /etc/apt/apt.conf.d/20auto-upgrades
+echo 'APT::Periodic::Update-Package-Lists "1";' | sudo tee --append  /etc/apt/apt.conf.d/20auto-upgrades
+echo 'APT::Periodic::Download-Upgradeable-Packages "1";' | sudo tee --append  /etc/apt/apt.conf.d/20auto-upgrades
+echo 'APT::Periodic::AutocleanInterval "7";' | sudo tee --append  /etc/apt/apt.conf.d/20auto-upgrades
+echo 'APT::Periodic::Unattended-Upgrade "1";' | sudo tee --append  /etc/apt/apt.conf.d/20auto-upgrades
