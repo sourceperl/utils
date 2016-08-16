b957e7a3f378359f7d570dfe21f9421617bfb7ad
l.lefebvre <loic.celine@free.fr>
2016-08-16 13:14:37 +0200

    Update setup_jessie_rm_big_packages.sh

--- jessie/setup_jessie_rm_big_packages.sh
+++ jessie/setup_jessie_rm_big_packages.sh
@@ -1,6 +1,6 @@
 # remove big packages from raspbian-jessie (gui version)
 
-sudo apt-get purge wolfram-engine scratch minecraft-pi sonic-pi gpicview penguinspuzzle 
+sudo apt-get purge wolfram-engine scratch minecraft-pi sonic-pi gpicview penguinspuzzle nodered
 sudo apt-get remove --purge libreoffice*
 sudo apt-get clean
 sudo apt-get autoremove
