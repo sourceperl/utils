00f756a8ee0cdadb607e123cda8ea5dcda833a79
l.lefebvre <loic.celine@free.fr>
2022-08-08 11:15:31 +0200

    Update for_all.sh

--- pkgs/for_all.sh
+++ pkgs/for_all.sh
@@ -2,4 +2,5 @@
 
 
 sudo apt install -y hexedit vim nmap lftp git minicom lsof pv \
-                    tree ntfs-3g supervisor ipython3 ack
+                    tree ntfs-3g supervisor ipython3 ack \
+                    ufw fail2ban
