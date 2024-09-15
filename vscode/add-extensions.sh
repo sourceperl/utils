a5c703bba596c1a92ed3f8e237e6e412c09ea641
sourceperl <3809271+sourceperl@users.noreply.github.com>
2024-09-15 16:04:03 +0200

    add "add-extensions.sh" script to vscode

---
+++ vscode/add-extensions.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+
+code --install-extension gruntfuggly.todo-tree
+code --install-extension ms-python.autopep8
+code --install-extension ms-python.black-formatter
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.isort
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+#code --install-extension tamasfe.even-better-toml
