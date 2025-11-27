8e523e7daff26f473bc22dadb230083e334440d9
sourceperl <3809271+sourceperl@users.noreply.github.com>
2025-11-27 12:14:35 +0100

    update vscode

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 
+# add vscode
+sudo apt install -y code
 
+# add extensions
 code --install-extension gruntfuggly.todo-tree
 code --install-extension ms-python.autopep8
 code --install-extension ms-python.black-formatter
@@ -9,3 +12,6 @@ code --install-extension ms-python.isort
 code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
 #code --install-extension tamasfe.even-better-toml
+
+# apply user settings
+install -vD settings.json ~/.config/Code/User/settings.json
